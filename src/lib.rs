237d572b24f810603eba9c95c4186fcf4d8a7b5e
Michael Meyer <ichmed95@gmail.com>
2023-01-11 00:03:32 +0100

    re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
 pub mod surreal;
 pub use surreal_macros::*;
+
+
+use surrealdb::{
+    Surreal, 
+    engine::remote::ws::Client, 
+    opt::auth::{
+        Root,
+        Database
+    }, 
+    sql::statements::SelectStatement};
+
