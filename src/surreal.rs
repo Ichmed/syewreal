dff5a09a1a48bfdfd81963157289e219cec000cb
Michael Meyer <ichmed95@gmail.com>
2023-01-11 16:57:12 +0100

    Add Suspense

--- src/surreal.rs
+++ src/surreal.rs
@@ -243,17 +243,22 @@ impl IntoPropValue<Parameters> for (&str, AttrValue) {
 #[derive(Properties, PartialEq)]
 pub struct SurrealContextProps {
     pub token: SurrealToken,
+    pub fallback: Option<Html>,
     pub children: Children,
 }
 
 #[function_component(SurrealContext)]
 pub fn surreal_context(props: &SurrealContextProps) -> Html {
     
+    let fallback = props.fallback.clone().unwrap_or(html!());
+
     if *props.token.ready {            
         html! {
-            <ContextProvider<SurrealToken> context={props.token.clone()}>
-                { for props.children.iter() }
-            </ContextProvider<SurrealToken>>
+            <Suspense {fallback}>
+                <ContextProvider<SurrealToken> context={props.token.clone()}>
+                    { for props.children.iter() }
+                </ContextProvider<SurrealToken>>
+            </Suspense>
         }
     }
     else {
